5895101edaa8870cd711bd2422a15d6e42281cd8
alamincoders <developeralamin03@gmail.com>
2023-09-09 20:16:17 +0600

    signin, sigup and forgor password created

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import Login from "@/components/Auth/Login";
+import SignUp from "@/components/Auth/SignUp";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/Auth/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="w-full h-full lg:h-screen grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8">
+      <div className="grid place-self-center">
+        <div>1</div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SignUp;
