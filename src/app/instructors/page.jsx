9e054177972895f28059b254510c7165231b057f
alamincoders <developeralamin03@gmail.com>
2023-09-11 13:46:39 +0600

    banner created

--- src/app/instructors/page.jsx
+++ src/app/instructors/page.jsx
@@ -2,6 +2,7 @@ import Pagination from "@/components/Common/Pagination";
 import Filters from "@/components/Screen/Instructor/Filters";
 import Hero from "@/components/Screen/Instructor/Hero";
 import InstructorLists from "@/components/Screen/Instructor/InstructorLists/InstructorLists";
+import JoinBannerForInstructors from "@/components/Screen/Instructor/JoinBanner";
 import React from "react";
 
 const InstructorPage = () => {
@@ -11,6 +12,7 @@ const InstructorPage = () => {
       <Filters />
       <InstructorLists />
       <Pagination />
+      <JoinBannerForInstructors />
     </>
   );
 };
