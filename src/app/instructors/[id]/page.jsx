9640f4455a67579169330dc3f1f8d49c7185a227
alamincoders <developeralamin03@gmail.com>
2023-09-11 21:39:06 +0600

    create instructor details

---
+++ src/app/instructors/[id]/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const InstructorDetailsPage = () => {
+  return (
+    <section className="mt-10 md:mt-20">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
+        <div className="border p-3">1</div>
+        <div className="border p-3">2</div>
+      </div>
+    </section>
+  );
+};
+p - 3;
+
+export default InstructorDetailsPage;
