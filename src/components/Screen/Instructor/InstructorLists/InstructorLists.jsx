a98c0e767ba3a73e3108e55c9115809d2d012889
alamincoders <developeralamin03@gmail.com>
2023-09-11 12:56:13 +0600

    updatated instructor lists

--- src/components/Screen/Instructor/InstructorLists/InstructorLists.jsx
+++ src/components/Screen/Instructor/InstructorLists/InstructorLists.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import InstructorList from "./InstructorList";
+import { instructorsDetail } from "../../../../../data/instructors";
 
 const InstructorLists = () => {
   return (
     <section className="mt-10">
       <div className="grid grid-cols-1  xl:grid-cols-2 gap-6">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <InstructorList key={idx} />
+        {instructorsDetail.map((instructor, idx) => (
+          <InstructorList key={idx} instructor={instructor} />
         ))}
       </div>
     </section>
