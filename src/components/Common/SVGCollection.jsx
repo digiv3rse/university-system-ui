a89529a4fc8604c0ea8de788f0ca5be0f348e2d8
alamincoders <developeralamin03@gmail.com>
2023-09-13 14:58:16 +0600

    star svg updated

--- src/components/Common/SVGCollection.jsx
+++ src/components/Common/SVGCollection.jsx
@@ -97,7 +97,7 @@ export const StarSVG = (props) => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6 text-amber-400 inline-block"
+          className="w-5 h-5 text-amber-400 inline-block"
         >
           <path
             strokeLinecap="round"
@@ -113,7 +113,7 @@ export const StarSVG = (props) => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6 text-amber-400 inline-block"
+          className="w-5 h-5 text-amber-400 inline-block"
         >
           <path
             strokeLinecap="round"
@@ -129,7 +129,7 @@ export const StarSVG = (props) => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6 text-amber-400 inline-block"
+          className="w-5 h-5 text-amber-400 inline-block"
         >
           <path
             strokeLinecap="round"
@@ -145,7 +145,7 @@ export const StarSVG = (props) => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6 text-amber-400 inline-block"
+          className="w-5 h-5 text-amber-400 inline-block"
         >
           <path
             strokeLinecap="round"
@@ -155,7 +155,7 @@ export const StarSVG = (props) => {
         </svg>
       </li>{" "}
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="w-6 h-6 text-amber-400 inline-block">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="w-5 h-5 text-amber-400 inline-block">
           <path
             fill="currentColor"
             d="m19.65 9.04l-4.84-.42l-1.89-4.45c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.73l3.67-3.18c.67-.58.32-1.68-.56-1.75zM12 15.4V6.1l1.71 4.04l4.38.38l-3.32 2.88l1 4.28L12 15.4z"
