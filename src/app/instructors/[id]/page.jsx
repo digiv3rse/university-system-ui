9e91233e4170e586c9930c86aa12648dc4d24895
alamincoders <developeralamin03@gmail.com>
2023-09-13 09:50:37 +0600

    instructor about

--- src/app/instructors/[id]/page.jsx
+++ src/app/instructors/[id]/page.jsx
@@ -1,12 +1,14 @@
+import InstructorAbout from "@/components/Screen/Instructor/InstructorDetails/InstructorAbout";
 import InstructorImage from "@/components/Screen/Instructor/InstructorDetails/InstructorImage";
 import React from "react";
 
 const InstructorDetailsPage = () => {
   return (
     <section className="mt-10 md:mt-20">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <div className="">
-            <InstructorImage/>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-8 lg:gap-8">
+        <div className="space-y-8">
+          <InstructorImage />
+          <InstructorAbout />
         </div>
         <div className="border p-3 col-span-2">2</div>
       </div>
---
+++ src/components/Screen/Instructor/InstructorDetails/InstructorAbout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InstructorAbout = () => {
+  return <div className="bg-white text-zinc-500 text-[0.94rem] leading-5 break-words text-center rounded-lg p-2 shadow-3xl">InstructorAbout</div>;
+};
+
+export default InstructorAbout;
