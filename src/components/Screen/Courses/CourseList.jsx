129a523c912156ac46f721090a962afda42fc07b
alamincoders <developeralamin03@gmail.com>
2023-09-13 18:00:59 +0600

    course page complete

--- src/components/Screen/Courses/CourseList.jsx
+++ src/components/Screen/Courses/CourseList.jsx
@@ -3,24 +3,22 @@ import { StarSVG } from "@/components/Common/SVGCollection";
 import Link from "next/link";
 import React from "react";
 
-const CourseList = () => {
+const CourseList = ({ course }) => {
+  const { name, imageSrc, rating, level, hours, minute, lectures } = course;
   return (
     <div className="text-zinc-500 text-[0.94rem] leading-5">
       <div className="h-full bg-white flex flex-col break-words rounded-lg shadow-3xl">
         <img
           className="w-full h-full rounded-tl-lg rounded-tr-lg align-middle overflow-clip object-fill object-center"
-          src="https://eduport.webestica.com/assets/images/courses/4by3/08.jpg"
+          src={imageSrc}
           alt=""
           loading="lazy"
         />
 
         <div className="flex-grow px-5 pt-4">
           <div className="flex justify-between mb-2">
-            <a
-              className="bg-violet-700/[0.1] text-violet-700 cursor-pointer text-[0.80rem] py-1 px-2 text-center rounded-md"
-              href="https://eduport.webestica.com/#"
-            >
-              All level
+            <a className="bg-violet-700/[0.1] text-violet-700 cursor-pointer text-[0.80rem] py-1 px-2 text-center rounded-md" href="#">
+              {level}
             </a>
 
             <Link className="text-zinc-800 cursor-pointer" href="#">
@@ -37,13 +35,13 @@ const CourseList = () => {
             </Link>
           </div>
 
-          <h5 className="text-zinc-800 text-[1.31rem] leading-7 font-bold mb-2">
-            <a className="cursor-pointer" href="https://eduport.webestica.com/#">
+          <h5 className="text-zinc-800 text-[1.31rem] leading-7 font-bold mb-2 hover:text-primary-600 duration-300 ease-in-out">
+            <Link className="cursor-pointer" href="/courses/id">
               Sketch from A to Z: for app designer
-            </a>
+            </Link>
           </h5>
 
-          <StarSVG className="items-center flex justify-start mb-4" />
+          <StarSVG className="items-center flex justify-start mb-4" rating={rating} />
         </div>
         <hr className="my-4" />
         <div className="rounded-bl-lg rounded-br-lg pb-4 px-4">
@@ -55,7 +53,7 @@ const CourseList = () => {
                   d="M12 20a8 8 0 0 0 8-8a8 8 0 0 0-8-8a8 8 0 0 0-8 8a8 8 0 0 0 8 8m0-18a10 10 0 0 1 10 10a10 10 0 0 1-10 10C6.47 22 2 17.5 2 12A10 10 0 0 1 12 2m.5 5v5.25l4.5 2.67l-.75 1.23L11 13V7h1.5Z"
                 />
               </svg>
-              12h 56m
+              {hours}h {minute}m
             </span>
 
             <span className="text-zinc-800">
@@ -65,7 +63,7 @@ const CourseList = () => {
                   d="M3.025 19V5H21v14H3.025Zm12.65-8H19V7h-3.325v4Zm-5.325 0h3.325V7H10.35v4Zm-5.325 0H8.35V7H5.025v4Zm0 6H8.35v-4H5.025v4Zm5.325 0h3.325v-4H10.35v4Zm5.325 0H19v-4h-3.325v4Z"
                 />
               </svg>
-              15 lectures
+              {lectures} lectures
             </span>
           </div>
         </div>
--- src/components/Screen/Courses/CourseLists.jsx
+++ src/components/Screen/Courses/CourseLists.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import CourseList from "./CourseList";
+import { courses } from "../../../../data/coursesData";
 
 const CourseLists = () => {
   return (
     <div className="mt-10">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8 ">
-        {Array.from({ length: 12 }).map((_, idx) => (
-          <CourseList key={idx} />
+        {courses.map((course, idx) => (
+          <CourseList key={idx} course={course} />
         ))}
       </div>
     </div>
