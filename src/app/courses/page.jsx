d3ca2c353a6cdca8fcd028c615bf548b4faca80f
alamincoders <developeralamin03@gmail.com>
2023-09-13 17:03:20 +0600

    courses added

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,3 +1,5 @@
+import CourseLists from "@/components/Screen/Courses/CourseLists";
+import Filters from "@/components/Screen/Courses/Filters";
 import Hero from "@/components/Screen/Courses/Hero";
 import React from "react";
 
@@ -5,6 +7,8 @@ const CoursesPage = () => {
   return (
     <>
       <Hero />
+      <Filters />
+      <CourseLists />
     </>
   );
 };
--- src/components/Screen/Courses/CourseLists.jsx
+++ src/components/Screen/Courses/CourseLists.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import CourseList from "./CourseList";
 
 const CourseLists = () => {
   return (
-    <div>CourseLists</div>
-  )
-}
+    <div className="mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8 ">
+        {Array.from({ length: 12 }).map((_, idx) => (
+          <CourseList key={idx} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default CourseLists
+export default CourseLists;
