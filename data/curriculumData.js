3e01262c6553655dba457eb03316846a5c359e8b
alamincoders <developeralamin03@gmail.com>
2023-09-13 23:28:34 +0600

    updated curriculum

---
+++ data/curriculumData.js
@@ -0,0 +1,100 @@
+export const curriculumData = [
+  {
+    title: "Introduction of Digital Marketing",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+      {
+        desc: "What is digital marketing",
+        minute: "15",
+        second: "10",
+      },
+      {
+        desc: "Type of digital marketing",
+        minute: "18",
+        second: "10",
+      },
+    ],
+    lecture: "3",
+  },
+  {
+    title: "Customer Life cycle",
+    lecture: "4",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+  {
+    title: "Customer Life cycle",
+    lecture: "4",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+  {
+    title: "Customer Life cycle",
+    lecture: "4",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+  {
+    title: "Customer Life cycle",
+    lecture: "4",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+  {
+    title: "Customer Life cycle",
+    lecture: "4",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+  {
+    title: "Customer Life cycle",
+    lecture: "4",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+  {
+    title: "Introduction of Digital Marketing",
+    lecture: "3",
+    details: [
+      {
+        desc: "Introduction",
+        minute: "2",
+        second: "10",
+      },
+    ],
+  },
+];
