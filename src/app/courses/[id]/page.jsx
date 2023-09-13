e2ebbc303ccdefdc3db9e77a3309ed4d53b139ab
alamincoders <developeralamin03@gmail.com>
2023-09-13 23:47:03 +0600

    updated

--- src/app/courses/[id]/page.jsx
+++ src/app/courses/[id]/page.jsx
@@ -7,6 +7,7 @@ import CourseTabs from "@/components/Screen/Courses/CourseDetails/CourseTabs";
 import CourseTags from "@/components/Screen/Courses/CourseDetails/CourseTags";
 import RecentViewsCourse from "@/components/Screen/Courses/CourseDetails/RecentViewsCourse";
 import Curriculum from "@/components/Screen/Courses/CourseDetails/Tabs/Curriculum";
+import Instructor from "@/components/Screen/Courses/CourseDetails/Tabs/Instructor";
 import Overview from "@/components/Screen/Courses/CourseDetails/Tabs/Overview";
 import TopListedCourses from "@/components/Screen/Courses/CourseDetails/TopListedCourses";
 import React, { useState } from "react";
@@ -22,6 +23,7 @@ const CourseDetailsPage = () => {
           <CourseTabs tabOpen={tabOpen} setTabOpen={setTabOpen} />
           {tabOpen === "Overview" ? <Overview /> : ""}
           {tabOpen === "Curriculum" ? <Curriculum /> : ""}
+          {tabOpen === "Instructor" ? <Instructor /> : ""}
         </div>
         <div>
           <CourseCard />
