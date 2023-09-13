41b61abf6fd119193615430586f3f5562b5e9ea6
alamincoders <developeralamin03@gmail.com>
2023-09-13 19:04:43 +0600

    updated

--- src/components/Screen/Home/TopCollege.jsx
+++ src/components/Screen/Home/TopCollege.jsx
@@ -10,8 +10,6 @@ import "swiper/css";
 
 import { Autoplay } from "swiper/modules";
 import Link from "next/link";
-import { topColleges } from "../../../../data/topColleges";
-import Image from "next/image";
 import Title from "@/components/Common/Title";
 import { courses } from "../../../../data/coursesData";
 import CourseList from "../Courses/CourseList";
@@ -19,16 +17,37 @@ import CourseList from "../Courses/CourseList";
 const TopCollege = () => {
   return (
     <section id="college" className="mt-10 md:mt-20">
-      <div className="text-zinc-500 flex flex-wrap text-[0.94rem] leading-5 mb-8">
-        <div className="mx-auto lg:px-3.5 text-center">
-          <Title>Top Listed Course</Title>
+      <div className="items-center text-zinc-500 md:flex space-y-3 md:space-y-0 text-[0.94rem] leading-5 justify-between">
+        <Title>Top Listed Courses</Title>
 
-          <p>Perceived end knowledge certainly day sweetness why cordially.</p>
+        <div>
+          <span className="mr-2">Want to see more?</span>
+
+          <Link
+            className="bg-sky-600/[0.1] text-sky-600 hover:text-white hover:bg-primary-400 cursor-pointer inline-block text-[0.81rem] font-medium py-1.5 px-3 text-center align-middle border border-solid rounded overflow-hidden "
+            href="/courses"
+          >
+            Go here
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              className="lucide lucide-chevron-right inline-block ml-1 -mt-0.5"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </Link>
         </div>
       </div>
 
       {/* cards */}
-      <div className="text-zinc-500 text-[0.94rem] leading-5 overflow-hidden">
+      <div className="text-zinc-500 text-[0.94rem] leading-5 overflow-hidden mt-5">
         <div className="relative">
           <div className="bottom-[42.90rem] left-[-9375.00rem] absolute right-[9447.44rem] top-0">
             slide <span>7 to 11</span>
