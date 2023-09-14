b4dd3fd0821f8d6d784c7f199c46060e47f33ed6
alamincoders <developeralamin03@gmail.com>
2023-09-14 10:06:31 +0600

    choose category section init ✅

--- src/components/Screen/Category/ChooseCategory.jsx
+++ src/components/Screen/Category/ChooseCategory.jsx
@@ -2,6 +2,7 @@
 import Title from "@/components/Common/Title";
 import Link from "next/link";
 import React from "react";
+import { courseCategories } from "../../../../data/courseCategories";
 
 const ChooseCategory = () => {
   return (
@@ -14,25 +15,9 @@ const ChooseCategory = () => {
 
       {/* ============================= choose category stated */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-        {Array.from({ length: 12 }).map((_, idx) => (
+        {courseCategories.map(({ name, imageSrc, bg_color, courseCount }, idx) => (
           <div key={idx} className="text-zinc-500 text-[0.94rem] leading-5">
-            <div className="bg-emerald-500/[0.1] break-words text-center rounded-lg p-6">
-              <div className="h-20 w-20 bg-white text-[1.90rem] leading-9 mb-4 mx-auto rounded-full flex items-center justify-center">
-                <img
-                  className="h-14 w-14 align-middle overflow-clip"
-                  src="https://eduport.webestica.com/assets/images/element/data-science.svg"
-                  alt=""
-                />
-              </div>
-
-              <h5 className="text-zinc-800 text-[1.31rem] leading-7 font-bold mb-2 hover:text-primary-600 duration-300 transition ease-in-out">
-                <Link className="cursor-pointer" href="#">
-                  Data Science
-                </Link>
-              </h5>
-
-              <h6 className="text-zinc-800 font-bold">15 Courses</h6>
-            </div>
+            {SingleCategory(bg_color, imageSrc, name, courseCount)}
           </div>
         ))}
       </div>
@@ -42,3 +27,22 @@ const ChooseCategory = () => {
 };
 
 export default ChooseCategory;
+
+// ================================= single category design here
+function SingleCategory(bg_color, imageSrc, name, courseCount) {
+  return (
+    <div style={{ backgroundColor: bg_color }} className={`break-words text-center rounded-lg p-6`}>
+      <div className="h-20 w-20 bg-white text-[1.90rem] leading-9 mb-4 mx-auto rounded-full flex items-center justify-center">
+        <img className="h-14 w-14 align-middle overflow-clip" src={imageSrc} alt="" />
+      </div>
+
+      <h5 className="text-zinc-800 text-[1.31rem] leading-7 font-bold mb-2 hover:text-primary-600 duration-300 transition ease-in-out">
+        <Link className="cursor-pointer" href="#">
+          {name}
+        </Link>
+      </h5>
+
+      <h6 className="text-zinc-800 font-bold">{courseCount} Courses</h6>
+    </div>
+  );
+}
