256be0bd4a040bd3de5299a177b015c82d3ce020
alamincoders <developeralamin03@gmail.com>
2023-09-14 09:40:31 +0600

    choose category stated

--- src/components/Screen/Category/ChooseCategory.jsx
+++ src/components/Screen/Category/ChooseCategory.jsx
@@ -1,7 +1,16 @@
+import Title from "@/components/Common/Title";
 import React from "react";
 
 const ChooseCategory = () => {
-  return <div className="mt-10 md:mt-20">ChooseCategory</div>;
+  return (
+    <div className="mt-10 md:mt-20">
+      <div className="text-zinc-500 text-[0.94rem] leading-5 px-3.5 text-center">
+        <Title>Choose a Categories</Title>
+
+        <p className="mt-4">Perceived end knowledge certainly day sweetness why cordially</p>
+      </div>
+    </div>
+  );
 };
 
 export default ChooseCategory;
