bc2bd1a6405caaf3eecb504fadd8cdd2da9497f9
alamincoders <developeralamin03@gmail.com>
2023-09-14 23:52:01 +0600

    updated event details hero

--- src/app/event/[id]/page.jsx
+++ src/app/event/[id]/page.jsx
@@ -1,7 +1,12 @@
+import EDetailsHero from "@/components/Screen/Event/EventDetails/EDetailsHero";
 import React from "react";
 
 const EventDetails = () => {
-  return <div>EventDetails</div>;
+  return (
+    <>
+      <EDetailsHero />
+    </>
+  );
 };
 
 export default EventDetails;
