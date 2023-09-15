e05812c9beb225de7639c73ad79b3f2980893eab
alamincoders <developeralamin03@gmail.com>
2023-09-15 21:54:41 +0600

    update faq

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const FaqPage = () => {
-  return <div>FaqPage</div>;
+  return (
+    <div className="bg-slate-200 rounded-lg py-12 flex items-center justify-center">
+      <p className="text-3xl">Coming Soon...</p>
+    </div>
+  );
 };
 
 export default FaqPage;
