869dfb13cbc89667888f0697632d03bb399db776
alamincoders <developeralamin03@gmail.com>
2023-09-15 08:47:41 +0600

    blog created

--- src/components/Screen/Blog/Blog.jsx
+++ src/components/Screen/Blog/Blog.jsx
@@ -2,29 +2,29 @@
 import Link from "next/link";
 import React from "react";
 
-const Blog = () => {
+const Blog = ({ blog }) => {
   return (
     <div className="text-zinc-500 text-[0.94rem] leading-5">
-      <div className="break-words relative rounded-lg">
-        <div className="rounded-lg overflow-hidden">
-          <img
-            className="w-full h-full align-middle rounded-lg overflow-clip"
-            src="https://eduport.webestica.com/assets/images/event/02.jpg"
-            alt=""
-          />
+      <div className="break-words rounded-lg">
+        <div className="rounded-lg overflow-hidden relative">
+          <img className="w-full h-full align-middle rounded-lg overflow-clip object-fill object-center" src={blog.imageSrc} alt="" />
 
-          <div className="items-start flex left-0 absolute top-0 z-[2] rounded-lg p-4">
-            <a className="bg-rose-600 text-white cursor-pointer text-[0.80rem] py-1 px-2 text-center rounded-md" href="#">
-              Student life
-            </a>
+          <div className="items-start flex left-0 absolute top-0 z-[2] rounded-lg p-4 w-full h-full bg-black/40">
+            <Link
+              style={{ backgroundColor: blog.bg_tag }}
+              className="text-white cursor-pointer text-[0.80rem] py-1 px-2 text-center rounded-md"
+              href="#"
+            >
+              {blog.tag}
+            </Link>
           </div>
         </div>
 
         <div className="flex-grow py-4 px-5">
           <h5 className="text-zinc-800 text-[1.31rem] leading-7 font-bold mb-2 hover:text-primary-600 duration-300 transition ease-in-out">
-            <a className="cursor-pointer" href="#">
-              Student Loan Survey: Many Owe $50K-plus
-            </a>
+            <Link className="cursor-pointer" href="/blog/id">
+              {blog.title.slice(0, 20)}...
+            </Link>
           </h5>
 
           <p
@@ -33,17 +33,17 @@ const Blog = () => {
               display: "-webkit-box",
             }}
           >
-            Affronting imprudence do he he everything. Offered chiefly farther of...
+            {blog.description.slice(0, 60)}...
           </p>
 
           <div className="flex justify-between">
             <h6 className="text-zinc-800 font-bold hover:text-primary-600 duration-300 transition ease-in-out">
-              <Link className="cursor-pointer" href="#">
-                Frances Guerrero
+              <Link className="cursor-pointer" href="/instructors/id">
+                {blog.author}
               </Link>
             </h6>
 
-            <span className="text-[0.82rem]">30M Ago</span>
+            <span className="text-[0.82rem]">{blog.issueDate}</span>
           </div>
         </div>
       </div>
--- src/components/Screen/Blog/Blogs.jsx
+++ src/components/Screen/Blog/Blogs.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Blog from "./Blog";
-import { courses } from "../../../../data/coursesData";
+import { blogs } from "../../../../data/blogs";
 
 const Blogs = () => {
   return (
     <div className="mt-10 md:mt-20">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8 ">
-        {courses.map((course, idx) => (
-          <Blog key={idx} course={course} />
+        {blogs.map((blog, idx) => (
+          <Blog key={idx} blog={blog} />
         ))}
       </div>
     </div>
