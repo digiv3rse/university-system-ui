5a1270e5e672f5771b7c76aa0edc6741966e515e
alamincoders <developeralamin03@gmail.com>
2023-09-16 20:25:57 +0600

    updated!

--- src/components/Screen/FAQ/Faqs.jsx
+++ src/components/Screen/FAQ/Faqs.jsx
@@ -4,7 +4,10 @@ const Faqs = () => {
   return (
     <div className="mt-10">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        <div className="lg:col-span-2"></div>
+        <div className="lg:col-span-2">
+          <h3 className="text-zinc-800 text-3xl font-bold">Frequently Asked Questions</h3>
+          <div className=""></div>
+        </div>
         <div></div>
       </div>
     </div>
