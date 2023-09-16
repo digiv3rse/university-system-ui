5b2befc8f6ad73e5db2519e805910c005a1c4cb1
alamincoders <developeralamin03@gmail.com>
2023-09-16 20:19:09 +0600

    faq hero design

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,10 +1,11 @@
+import FHero from "@/components/Screen/FAQ/FHero";
 import React from "react";
 
 const FaqPage = () => {
   return (
-    <div className="bg-slate-200 rounded-lg py-12 flex items-center justify-center">
-      <p className="text-3xl">Coming Soon...</p>
-    </div>
+    <>
+      <FHero />
+    </>
   );
 };
 
