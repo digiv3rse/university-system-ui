4c8a192cdb03e06c92fc5d89ff46a15c7be57833
alamincoders <developeralamin03@gmail.com>
2023-09-16 20:22:58 +0600

    updated faqs

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,10 +1,12 @@
 import FHero from "@/components/Screen/FAQ/FHero";
+import Faqs from "@/components/Screen/FAQ/Faqs";
 import React from "react";
 
 const FaqPage = () => {
   return (
     <>
       <FHero />
+      <Faqs />
     </>
   );
 };
---
+++ src/components/Screen/FAQ/Faqs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Faqs = () => {
+  return (
+    <div className="mt-10">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2"></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
